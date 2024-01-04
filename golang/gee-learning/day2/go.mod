1f27b5e0ca6396a0654b596437860fa6a4b1b886
Bocabbage <378066505@qq.com>
2024-01-05 07:54:23 +0800

    7day-golang project: gee learning.

---
+++ golang/gee-learning/day2/go.mod
@@ -0,0 +1,7 @@
+module bocabbage-learning/gee
+
+go 1.21.3
+
+require gee v0.0.0
+
+replace gee => ./gee
---
+++ golang/gee-learning/day2/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"gee"
+	"net/http"
+)
+
+func main() {
+	r := gee.New()
+	r.GET("/", func(c *gee.Context) {
+		c.HTML(
+			http.StatusOK,
+			"<h1>Hello my friend!</h1>",
+		)
+	})
+
+	r.POST("/login", func(c *gee.Context) {
+		c.JSON(
+			http.StatusOK, gee.H{
+				"username": c.PostForm("username"),
+				"password": c.PostForm("password"),
+			})
+	})
+
+	r.Run(":9999")
+}
