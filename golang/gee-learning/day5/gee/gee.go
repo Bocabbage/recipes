65b0cc72230bc574b0624a3212574e38c2ef0b78
Bocabbage <378066505@qq.com>
2024-01-11 18:03:09 +0800

    Update gee-day5(unfinished).

---
+++ golang/gee-learning/day5/gee/gee.go
@@ -0,0 +1,67 @@
+package gee
+
+import (
+	"net/http"
+)
+
+type HandlerFunc func(*Context)
+
+type (
+	RouterGroup struct {
+		prefix      string
+		middlewares []HandlerFunc
+		parent      *RouterGroup
+		engine      *Engine
+	}
+
+	Engine struct {
+		*RouterGroup
+		router *router
+		groups []*RouterGroup // store all groups
+	}
+)
+
+func New() *Engine {
+	engine := &Engine{router: newRouter()}
+	engine.RouterGroup = &RouterGroup{engine: engine}
+	engine.groups = []*RouterGroup{engine.engine.RouterGroup}
+	return engine
+}
+
+func (group *RouterGroup) Group(prefix string) *RouterGroup {
+	engine := group.engine
+	newGroup := &RouterGroup{
+		prefix: group.prefix + prefix,
+		parent: group,
+		engine: engine,
+	}
+	engine.groups = append(engine.engine.groups, newGroup)
+	return newGroup
+}
+
+func (group *RouterGroup) addRouter(method string, pattern string, handler HandlerFunc) {
+	fullPattern := group.prefix + pattern
+	group.engine.router.addRoute(method, fullPattern, handler)
+}
+
+func (group *RouterGroup) GET(pattern string, handler HandlerFunc) {
+	group.addRouter("GET", pattern, handler)
+}
+
+func (group *RouterGroup) POST(pattern string, handler HandlerFunc) {
+	group.addRouter("POST", pattern, handler)
+}
+
+func (group *RouterGroup) Use(middlewares ...HandlerFunc) {
+	group.middlewares = append(group.middlewares, middlewares...)
+}
+
+func (engine *Engine) Run(addr string) (err error) {
+	return http.ListenAndServe(addr, engine)
+}
+
+// Implement the http.Handler interface for Engine
+func (engine *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	c := newContext(w, req)
+	engine.router.handle(c)
+}
