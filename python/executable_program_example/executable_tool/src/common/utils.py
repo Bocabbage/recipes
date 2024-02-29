0a5251cd95b31ec69d682e8e1169c5bd9fae7f44
Bocabbage <378066505@qq.com>
2024-02-29 18:10:18 +0800

    Add python package setup demo.

---
+++ python/executable_program_example/executable_tool/src/common/utils.py
@@ -0,0 +1,2 @@
+def test_path_add(a, b):
+    return a + b
---
+++ python/executable_program_example/executable_tool/src/execute_pacakge/exe_entrypoint.py
@@ -0,0 +1,8 @@
+from common.utils import test_path_add
+
+def hello_world():
+    print("Hello, world!")
+    print(f"And test 4+4: {test_path_add(4, 4)}")
+
+def another_exe():
+    print("Another exe!")
